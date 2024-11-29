d8a6335b1077c8cf8597315cc3f9550320e37e0a
s3rch <sermoba8@gmail.com>
2024-11-29 16:08:05 +0100

    prueba linea

--- main.c
+++ main.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "parser.h"
 
 
-
+void executeLine(tline *line)
+{
+    printf(":)\n");
+}
 
 int main(void) {
     while (1) {
-        tline *line= tokenize("hola");
+
         char buf[512];
         printf("msh> ");
-        fgets(buf,512,stdin);
+
+
         if (fgets(buf,512,stdin) == NULL) {
             printf("ERROR, se ha producido un error");
             break;
         }
-        printf("hola");
+        if (strlen(buf) == 0)
+        {
+            continue;
+        }
+
+        tline *line = tokenize(buf);
+
+        if (line)
+        {
+            executeLine(line);
+        }
+
+
+
 
     }
 
